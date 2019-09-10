d9f845270c57d60e266d7cb920485163793fc7e1
Matthew <mattg1827@gmail.com>
2019-09-09 18:27:13 -0700

    Help me Ray

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  port: process.env.PORT || 3306,
+  if (process.env.JAWSSB_URL){
+    connection = mysql.createConnection(process.env.JAWSSB_URL)
+  }else{
+  connection = mysql.createConnection({
+  // port: process.env.PORT || 3306,
   host: "localhost",
   user: "nodeuser",
   password: "password",
   database: "burgers_db"
 });
+};
 
 // Make connection.
 connection.connect(function(err) {
@@ -19,4 +24,5 @@ connection.connect(function(err) {
 });
 
 // Export connection for our ORM to use.
+
 module.exports = connection;
